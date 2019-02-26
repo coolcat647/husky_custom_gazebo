545269971a2cc60037b825841e3c83738e6bc1f8
samliu <c52647w@gmail.com>
2019-02-26 20:16:26 +0800

    create gazebo husky

--- catkin_ws/build/husky/husky_desktop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samliu/Documents/husky_custom_gazebo/catkin_ws/src/husky/husky_desktop
-# Build directory: /home/samliu/Documents/husky_custom_gazebo/catkin_ws/build/husky/husky_desktop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/devel/share/husky_msgs/cmake/husky_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(husky_msgs_MESSAGE_FILES "/home/samliu/Documents/husky_custom_gazebo/catkin_ws/src/husky/husky_msgs/msg/HuskyStatus.msg")
-set(husky_msgs_SERVICE_FILES "")
